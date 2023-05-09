23106eb3b361669289ba6e05e6a0cba2affaae38
siralfbaez <siralf@urimagination.com>
2023-05-09 09:33:02 -0400

    3rd quick hello commit

---
+++ src/com/urimagination/Main.java
@@ -0,0 +1,14 @@
+package com.urimagination;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int age = 30;
+        Date now = new Date();
+        System.out.println(age);
+        System.out.println(now);
+    }
+}
